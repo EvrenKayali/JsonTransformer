c7591a344a3caee5142b9788dcc74a9f98a8c362
Evren Kayali <evren.kayali@readify.net>
2018-08-27 20:06:22 +1000

    nuspec file changed for depoloyment

--- src/JsonTransformer/FieldValueConverters/IntFieldConverter.cs
+++ src/JsonTransformer/FieldValueConverters/IntFieldConverter.cs
@@ -1,7 +1,7 @@
 ﻿namespace JsonTransformer.FieldValueConverters
 {
-
     using Newtonsoft.Json.Linq;
+
     public class IntFieldConverter : IFieldValueConverter
     {
         public JToken Convert(JToken value)
